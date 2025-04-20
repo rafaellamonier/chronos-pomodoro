d374718d5effe12c3c5723ac0eca76c9c8ff5035
rafaellamonier <rafaellamonier17@hotmail.com>
2025-04-19 21:05:57 -0300

    wip src and index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<h1 className="text-blue-500">Olá mundo!</h1>
 	</StrictMode>,
 );
